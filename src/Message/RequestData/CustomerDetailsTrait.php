11addb574cc2108a4802b62afb06afe2251bd6f3
Andreas Lange <andreas.lange@connox.de>
2016-03-10 11:55:48 +0100

    Small optimizations

--- src/Message/RequestData/CustomerDetailsTrait.php
+++ src/Message/RequestData/CustomerDetailsTrait.php
@@ -74,29 +74,9 @@ trait CustomerDetailsTrait
      */
     protected function appendCustomerDetails(SimpleXMLElement $data)
     {
-        $card = $this->getCard();
-        $customer = $this->getCustomerDetails();
-
         $data->addChild('customer_details');
-        $data->customer_details[0]['customerid'] = $customer->getId();
-        $data->customer_details[0]['customertype'] = $customer->getType();
-        $data->customer_details[0]['salutation'] = $card->getGender();
-        $data->customer_details[0]['title'] = $card->getBillingTitle();
-        $data->customer_details[0]['firstName'] = $card->getBillingFirstName();
-        $data->customer_details[0]['lastName'] = $card->getBillingLastName();
-        $data->customer_details[0]['street'] = $card->getBillingAddress1();
-        $data->customer_details[0]['streetNo'] = null;
-        $data->customer_details[0]['addressAddition'] = $card->getBillingAddress2();
-        $data->customer_details[0]['zip'] = $card->getBillingPostcode();
-        $data->customer_details[0]['city'] = $card->getBillingCity();
-        $data->customer_details[0]['country'] = $this->getCountryCode($card->getBillingCountry());
-        $data->customer_details[0]['email'] = $card->getEmail();
-        $data->customer_details[0]['phone'] = $card->getBillingPhone();
-        $data->customer_details[0]['cellPhone'] = null;
-        $data->customer_details[0]['birthday'] = $card->getBirthday('Ymd');
-        $data->customer_details[0]['language'] = $customer->getLanguage();
-        $data->customer_details[0]['ip'] = $this->getClientIp();
-        $data->customer_details[0]['customerGroup'] = $customer->getGroup();
+        $this->appendCustomerDetailsAdditional($data, $this->getCustomerDetails());
+        $this->appendCustomerDetailsCard($data, $this->getCard());
     }
 
     /**
@@ -121,4 +101,43 @@ trait CustomerDetailsTrait
      * @codeCoverageIgnore
      */
     abstract protected function setParameter($key, $value);
+
+    /**
+     * Fills additional data
+     *
+     * @param SimpleXMLElement $data
+     * @param Customer         $customer
+     */
+    private function appendCustomerDetailsAdditional(SimpleXMLElement $data, Customer $customer)
+    {
+        $data->customer_details[0]['customerid'] = $customer->getId();
+        $data->customer_details[0]['customertype'] = $customer->getType();
+        $data->customer_details[0]['language'] = $customer->getLanguage();
+        $data->customer_details[0]['ip'] = $this->getClientIp();
+        $data->customer_details[0]['customerGroup'] = $customer->getGroup();
+    }
+
+    /**
+     * Fills the card information
+     *
+     * @param SimpleXMLElement $data
+     * @param CreditCard       $card
+     */
+    private function appendCustomerDetailsCard(SimpleXMLElement $data, CreditCard $card)
+    {
+        $data->customer_details[0]['salutation'] = $card->getGender();
+        $data->customer_details[0]['title'] = $card->getBillingTitle();
+        $data->customer_details[0]['firstName'] = $card->getBillingFirstName();
+        $data->customer_details[0]['lastName'] = $card->getBillingLastName();
+        $data->customer_details[0]['street'] = $card->getBillingAddress1();
+        $data->customer_details[0]['streetNo'] = null;
+        $data->customer_details[0]['addressAddition'] = $card->getBillingAddress2();
+        $data->customer_details[0]['zip'] = $card->getBillingPostcode();
+        $data->customer_details[0]['city'] = $card->getBillingCity();
+        $data->customer_details[0]['country'] = $this->getCountryCode($card->getBillingCountry());
+        $data->customer_details[0]['email'] = $card->getEmail();
+        $data->customer_details[0]['phone'] = $card->getBillingPhone();
+        $data->customer_details[0]['cellPhone'] = null;
+        $data->customer_details[0]['birthday'] = $card->getBirthday('Ymd');
+    }
 }
